a190061bc49f1171ca7a4f99acdd607e8de6af38
Egidijus Jucevičius <egidijusj@wix.com>
2019-03-02 12:27:58 +0200

    cleanup

--- src/react/App.js
+++ src/react/App.js
@@ -21,52 +21,44 @@ class App extends Component {
     };
   }
 
-  toggle(i) {
-    this.setState(({ todos }) => ({
-      todos: toggle(todos, todos[i])
-    }));
-  }
-
-  toggleAll(completed) {
-    this.setState(({ todos }) => ({
-      todos: toggleAll(todos, completed)
-    }));
-  }
-
-  rename(i, name) {
-    this.setState(({ todos }) => ({
-      todos: rename(todos, todos[i], name)
-    }));
-  }
-
-  remove(i) {
-    this.setState(({ todos }) => ({
-      todos: remove(todos, todos[i])
-    }));
-  }
-
   render() {
     const { todos, activeFilter } = this.state;
-    const filteredTodos = filter(todos, activeFilter);
-    const remainingItems = countRemaining(todos);
     return (
       <section className="todoapp">
         <div>
           <Header onAdd={name => this.setState({ todos: add(todos, name) })} />
+
           {todos.length > 0 && (
             <Todos
-              todos={filteredTodos}
-              toggle={i => this.toggle(i)}
-              toggleAll={completed => this.toggleAll(completed)}
-              rename={(i, name) => this.rename(i, name)}
-              remove={i => this.remove(i)}
+              todos={filter(todos, activeFilter)}
+              toggle={i =>
+                this.setState(({ todos }) => ({
+                  todos: toggle(todos, todos[i])
+                }))
+              }
+              toggleAll={completed =>
+                this.setState(({ todos }) => ({
+                  todos: toggleAll(todos, completed)
+                }))
+              }
+              rename={(i, name) =>
+                this.setState(({ todos }) => ({
+                  todos: rename(todos, todos[i], name)
+                }))
+              }
+              remove={i =>
+                this.setState(({ todos }) => ({
+                  todos: remove(todos, todos[i])
+                }))
+              }
             />
           )}
+
           {todos.length > 0 && (
             <Footer
               activeFilter={activeFilter}
               onFilterChange={filter => this.setState({ activeFilter: filter })}
-              remainingItems={remainingItems}
+              remainingItems={countRemaining(todos)}
             />
           )}
         </div>
